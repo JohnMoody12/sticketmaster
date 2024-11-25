2693f450914ba95247603911db1592e0cec62654
JohnMoody12 <123786549+JohnMoody12@users.noreply.github.com>
2024-11-24 21:50:54 -0600

    msg

---
+++ backend/search.js
@@ -0,0 +1,71 @@
+// let arr = [7, 2, 4, 5];
+
+// Array.prototype.myReduce = function (callbackFn, initialValue) {
+//   const noInit = initialValue === undefined;
+//   const l = this.length;
+
+//   if (noInit && l === 0) {
+//     throw new TypeError("Reduce empty w/no initial value");
+//   }
+
+//   let acc = noInit ? this[0] : initialValue;
+//   let initIndex = noInit ? 1 : 0;
+
+//   for (let k = initIndex; k < l; k++) {
+//     if (Object.hasOwn(this, k)) {
+//       acc = callbackFn(acc, this[k], k, this);
+//     }
+//   }
+//   return acc;
+// };
+
+// console.log(arr.myReduce((acc) => acc + 1, 0));
+function debounce(func, wait = 0) {
+  let timeoutId = null;
+  let lastArgs = null;
+  let lastContext = null;
+  function debounced(...args) {
+    lastContext = this;
+    lastArgs = args;
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(function () {
+      timeoutId = null;
+      func.apply(lastContext, lastArgs);
+      lastContext = null;
+      lastArgs = null;
+    }, wait);
+  }
+
+  debounced.cancel = function () {
+    clearTimeout(timeoutId);
+    timeoutId = null;
+  };
+
+  debounced.flush = function () {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+      timeoutId = null;
+      func.apply(lastContext, lastArgs);
+      lastContext = null;
+      lastArgs = null;
+    }
+  };
+
+  return debounced;
+}
+i = 0;
+function inc() {
+  i++;
+  console.log(i);
+}
+
+const dI = debounce(inc, 2000);
+
+dI();
+dI.flush();
+dI();
+dI.flush();
+dI();
+dI.flush();
+dI();
+setTimeout(() => dI(), 2000);
