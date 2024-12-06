bab60610016e9511af02ba0c1021c1d11a7f341f
JohnMoody12 <123786549+JohnMoody12@users.noreply.github.com>
2024-12-05 22:33:50 -0600

    a

---
+++ backend/a.js
@@ -0,0 +1,11 @@
+const createCounter = () => {
+  let count = 0;
+  return function () {
+    count += 1;
+    return count;
+  };
+};
+
+const counter = createCounter();
+console.log(counter()); // Outputs: 1
+console.log(counter()); // Outputs: 2
--- backend/search.js
+++ backend/search.js
@@ -117,57 +117,131 @@ showThisStandalone.apply(person); // `undefined` because all parts of a class' b
 // person2.showThis();
 // person2.showThis2();
 
-const john = {
-  age: 42,
-  getAge: function () {
-    return this.age;
-  },
-};
+// const john = {
+//   age: 42,
+//   getAge: function () {
+//     return this.age;
+//   },
+// };
 
-console.log(john.getAge()); // 42
+// console.log(john.getAge()); // 42
 
-const unboundGetAge = john.getAge;
-console.log(unboundGetAge()); // undefined
+// const unboundGetAge = john.getAge;
+// console.log(unboundGetAge()); // undefined
 
-const boundGetAge = john.getAge.bind(john);
-console.log(boundGetAge()); // 42
+// const boundGetAge = john.getAge.bind(john);
+// console.log(boundGetAge()); // 42
 
-const mary = { age: 21 };
-const boundGetAgeMary = john.getAge.bind(mary);
-console.log(boundGetAgeMary()); // 21
+// const mary = { age: 21 };
+// const boundGetAgeMary = john.getAge.bind(mary);
+// console.log(boundGetAgeMary()); // 21
 
-async function bob() {
-  console.log("1");
-  await Promise.resolve();
-  console.log("2");
-}
+// async function bob() {
+//   console.log("1");
+//   await Promise.resolve();
+//   console.log("2");
+// }
 
-console.log("A");
-bob();
-console.log("B");
+// console.log("A");
+// bob();
+// console.log("B");
 
-function foo() {}
+// function foo() {}
 
-// foo as a function object:
-console.log(foo.__proto__ === Function.prototype); // true
+// // foo as a function object:
+// console.log(foo.__proto__ === Function.prototype); // true
 
-// foo's .prototype property:
-console.log(foo.prototype.__proto__ === Object.prototype); // true
+// // foo's .prototype property:
+// console.log(foo.prototype.__proto__ === Object.prototype); // true
 
-// To really show the difference:
-console.log(foo.__proto__ === foo.prototype); // false
+// // To really show the difference:
+// console.log(foo.__proto__ === foo.prototype); // false
 
-console.log(Object.getPrototypeOf(foo.__proto__));
-console.log(Object.getPrototypeOf(foo.prototype));
+// console.log(Object.getPrototypeOf(foo.__proto__));
+// console.log(Object.getPrototypeOf(foo.prototype));
 
-function Person(name, age) {
-  this.name = name;
-  this.age = age;
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
 
-  this.sayHello = function () {
-    console.log(`Hi I'm ${this.name} and I'm ${this.age}`);
+//   this.sayHello = function () {
+//     console.log(`Hi I'm ${this.name} and I'm ${this.age}`);
+//   };
+
+//   this.sayGoodBye = function () {
+//     console.log("baiiii");
+//   };
+// }
+
+// Person.prototype.sayGoodBye = function () {
+//   console.log("Buh Bye!");
+// };
+
+// let joe = new Person("Joe", 20);
+// joe.sayHello();
+// joe.sayGoodBye();
+
+// const createCounter = () => {
+//   let count = 0;
+//   return () => {
+//     count += 1;
+//     return count;
+//   };
+// };
+
+// const counter = createCounter();
+// console.log(counter()); // Outputs: 1
+// console.log(counter()); // Outputs: 2
+
+class Example {
+  constructor(name) {
+    this.name = name;
+  }
+
+  // Normal function: `this` refers to the class instance when called as obj.method()
+  normalFunction() {
+    console.log("normalFunction this.name:", this.name); // "Alice" if called on obj = new Example("Alice")
+
+    // Inner function as a normal function:
+    function innerNormalFunction() {
+      //console.log("inner normal ", this.name);
+    }
+    innerNormalFunction();
+
+    // To preserve `this` inside a normal function:
+    const that = this;
+    function innerNormalWithThat() {
+      console.log("innerNormalWithThat that.name:", that.name); // Correctly logs "Alice"
+    }
+    innerNormalWithThat();
+
+    // Arrow function inside normal function:
+    const innerArrowFunction = () => {
+      // The `this` here is inherited from normalFunction's `this`, which is the instance.
+      console.log("innerArrowFunction this.name:", this.name); // "Alice"
+    };
+    innerArrowFunction();
+  }
+
+  // Arrow function at the class level:
+  arrowFunction = () => {
+    // `this` here also refers to the instance due to lexical scoping.
+    console.log("arrowFunction this.name:", this.name); // "Alice"
   };
 }
 
-let joe = new Person("Joe", 20);
-joe.sayHello();
+// Usage:
+const obj = new Example("Alice");
+obj.normalFunction();
+obj.arrowFunction();
+
+function Timer() {
+  this.seconds = 0;
+  let that = this;
+  setInterval(function () {
+    that.seconds++;
+    console.log(that.seconds);
+  }, 1000);
+}
+
+const timer = new Timer();
