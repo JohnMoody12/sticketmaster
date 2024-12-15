3e14d1fd1a8e361d65510347c9c069f9925d9920
JohnMoody12 <123786549+JohnMoody12@users.noreply.github.com>
2024-12-15 17:16:05 -0600

    own version

--- frontend/vite-project/src/TicTac.jsx
+++ frontend/vite-project/src/TicTac.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const WINNER = [
+const WIN_CONDITIONS = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -10,91 +10,69 @@ const WINNER = [
   [2, 5, 8],
 
   [0, 4, 8],
-  [2, 4, 6],
+  [6, 4, 2],
 ];
 
-export function Cell({ idx, disabled, mark, turn, onClick }) {
+export const Cell = ({ idx, markCell, board }) => {
   return (
     <button
-      className=" hover:bg-slate-200 border-black border-2 aspect-square w-24 h-24 text-6xl"
-      aria-label={mark == null ? `Mark cell ${idx} as ${turn}` : undefined}
-      disabled={disabled}
-      onClick={onClick}
+      className="bg-slate-300 border-2 hover:bg-slate-200 text-6xl"
+      style={{ width: "8vw", height: "8vw" }}
+      onClick={() => markCell(idx)}
+      aria-label={`Cell ${idx}`}
     >
-      <span aria-hidden={true}>{mark}</span>
+      {board[idx]}
     </button>
   );
-}
+};
 
 export const TicTac = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [xTurn, setXTurn] = useState(true);
+  const [board, setBoard] = useState(new Array(9).fill(null));
+  const [xTurn, setXturn] = useState(true);
   const [winner, setWinner] = useState(null);
 
-  function checkWinner(board) {
-    for (let wins of WINNER) {
-      const [a, b, c] = wins;
-      if (board[a] != null && board[a] == board[b] && board[a] == board[c]) {
-        return board[a];
+  function reset() {
+    if (winner == null && !window.confirm("Are you sure?")) {
+      return;
+    }
+    setBoard(new Array(9).fill(null));
+    setXturn(true);
+    setWinner(null);
+  }
+  function markCell(idx) {
+    let player = xTurn ? "X" : "O";
+    let newBoard = board.slice();
+    if (newBoard[idx] != null) {
+      return;
+    }
+    newBoard[idx] = player;
+    setBoard(newBoard);
+    setXturn(!xTurn);
+    checkWinner(player, newBoard);
+  }
+
+  function checkWinner(player, newBoard) {
+    for (let winCondition of WIN_CONDITIONS) {
+      const [a, b, c] = winCondition;
+      if (
+        newBoard[a] != null &&
+        newBoard[a] == newBoard[b] &&
+        newBoard[a] == newBoard[c]
+      ) {
+        setWinner(player);
       }
     }
-    return null;
   }
-  //let winner = checkWinner(board);
-  function status() {
-    if (winner) {
-      return `${winner} wins!`;
-    } else if (!board.includes(null)) {
-      return `draw`;
-    } else {
-      return `${turn}'s turn`;
-    }
-  }
-
-  let turn = xTurn ? "X" : "O";
 
   return (
-    <div className="flex flex-col items-center">
-      <div>{status()}</div>
-      <div className=" grid grid-cols-3 rows-3 gap-1 mx-auto">
-        {Array(9)
-          .fill(null)
-          .map((_, idx) => {
-            return (
-              <Cell
-                key={idx}
-                idx={idx}
-                turn={turn}
-                mark={board[idx]}
-                disabled={board[idx] != null}
-                onClick={() => {
-                  const newBoard = board.slice();
-                  newBoard[idx] = turn;
-                  setBoard(newBoard);
-                  let newWinner = checkWinner(newBoard);
-                  if (newWinner) {
-                    setWinner(newWinner);
-                  }
-                  setXTurn(!xTurn);
-                }}
-              />
-            );
-          })}
+    <div>
+      {winner ? `${winner} has won` : `Player ${xTurn ? "X" : "O"}'s turn`}
+      <div className=" grid grid-cols-3 grid-rows-3 w-fit mx-auto">
+        {new Array(9).fill(null).map((v, idx) => {
+          return <Cell idx={idx} key={idx} markCell={markCell} board={board} />;
+        })}
       </div>
-      <button
-        className="border-2 m-4 rounded-md bg-slate-500 text-white text-3xl p-3 hover:bg-slate-400"
-        onClick={() => {
-          if (winner == null) {
-            const confirm = window.confirm("Are you sure?");
-          }
-          if (confirm) {
-            setBoard(Array(9).fill(null));
-            setXTurn(true);
-          }
-        }}
-      >
-        {winner ? "Play Again?" : "Reset"}
-      </button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 };
